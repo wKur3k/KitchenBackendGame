7f3446f20d0957e13fb92823d491898f3184654e
wKur3k <kurek.wojciech.01@gmail.com>
2021-08-25 06:20:20 +0200

    Added required NuGet Packages

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//comment
+
 namespace SimpleBackendGame
 {
     public class Startup
@@ -27,6 +27,7 @@ namespace SimpleBackendGame
         {
 
             services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
